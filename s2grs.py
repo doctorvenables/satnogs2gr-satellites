39d788e55fec1f25f16e13d1c7b0b07342d2913f
doctorvenables <doctorvenables@gmail.com>
2020-07-22 11:13:52 +0100

    added argument correctly for kiss_output

--- s2grs.py
+++ s2grs.py
@@ -63,8 +63,8 @@ print("Starting gr-satellites processing for ",sat_name)
 wav_arg="--wavfile="+wav_name
 exec_string= '/usr/local/bin/gr_satellites'
 now_kss=datetime.datetime.now()
-kiss_arg=out_path+'/data/kiss_'+now_kss.strftime("%Y-%m-%d-%H-%M-%S")+'.kss'
-exec_arg0=[exec_string,str(norad_id),wav_arg,"--samp_rate=48e3","--clk_limit=0.03"]
+kiss_arg="--kiss_out="+out_path+'/data/'+str(norad_id)+'kiss_'+now_kss.strftime("%Y-%m-%d-%H-%M-%S")+'.kss'
+exec_arg0=[exec_string,str(norad_id),wav_arg,"--samp_rate=48e3","--clk_limit=0.03",kiss_arg]
 print('Kiss arg=',kiss_arg)
 #satnogs bpsk observations require an offset of 12e3
 if ('bpsk' in script_name):
